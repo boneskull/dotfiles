99a86528d319281066947ca9207c32bcb86e7245
Christopher Hiller <boneskull@boneskull.com>
2016-01-11 12:43:01 -0800

    config git user via bootstrrap

--- bootstrap.bash
+++ bootstrap.bash
@@ -4,31 +4,37 @@ git pull origin master;
 
 function doIt() {
   local VUNDLE="${HOME}/.vim/bundle/Vundle.vim"
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.bash" \
+  local GIT_AUTHOR_EMAIL="boneskull@boneskull.com"
+  local GIT_AUTHOR_NAME="Christopher Hiller"
+
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.bash" \
     --exclude ".idea" --exclude "README.md" --exclude "LICENSE" -avh \
     --no-perms . ${HOME};
 
-	# clone/update Vundle
-	if [[ -d ${VUNDLE} ]] 
-	then
-          cd ${VUNDLE}
-          git pull
-	else
-	  git clone https://github.com/gmarik/Vundle.vim.git ${VUNDLE}
-	fi
-	vim +PluginInstall +qall
+  # clone/update Vundle
+  if [[ -d ${VUNDLE} ]]
+  then
+    cd ${VUNDLE}
+    git pull --rebase
+  else
+    git clone https://github.com/gmarik/Vundle.vim.git ${VUNDLE}
+  fi
+  vim +PluginInstall +qall
 
+  # setup git
+  git config --global user.email "$GIT_AUTHOR_EMAIL" 2&1>/dev/null
+  git config --global user.name "$GIT_AUTHOR_NAME" 2&1>/dev/null
 
-	source ${HOME}/.bash_profile;
+  source ${HOME}/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+  doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+  echo "";
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt;
+  fi;
 fi;
 unset doIt;
