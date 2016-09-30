8d5e50607b9c2c8f5567de14aeadd87fadfbf79b
Christopher Hiller <boneskull@boneskull.com>
2016-09-30 12:26:05 -0700

    fix broken brewfile location

--- tag-osx/exports.darwin.zsh
+++ tag-osx/exports.darwin.zsh
@@ -1,6 +1,6 @@
 whence atom >/dev/null && export EDITOR="/usr/bin/env atom -w"
 
 export HOMEBREW_CASK_OPTS="--appdir=~/Applications";
-export HOMEBREW_BREWFILE="~/.Brewfile"
+export HOMEBREW_BREWFILE="${HOME}/.Brewfile"
 
 [[ -d /usr/local/share/zsh/help ]] && export HELPDIR=/usr/local/share/zsh/help
