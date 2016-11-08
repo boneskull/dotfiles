693ffc592e2930135124bb213ffd93ae3ca188c3
Christopher Hiller <boneskull@boneskull.com>
2016-11-08 13:51:06 -0800

    add Dash/man integration

--- tag-osx/aliases.darwin.zsh
+++ tag-osx/aliases.darwin.zsh
@@ -9,3 +9,13 @@ whence rcup >/dev/null && {
   alias rcup="rcup -t app git osx"
   alias lsrc="lsrc -t app git osx"
 }
+
+[[ -d /Applications/Dash.app || -d ${HOME}/Applications/Dash.app ]] && {
+  # Man pages to Dash
+  dash_man () {
+    /usr/bin/open "dash://manpages:$(omz_urlencode ${@})"
+  }
+
+  alias man=dash_man
+}
+
