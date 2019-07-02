93991f5ed29786813d849474607fb26b6c70599d
Christopher Hiller <boneskull@boneskull.com>
2019-07-02 14:00:31 -0700

    fix (?) ruby gem executable path

--- tag-osx/exports.darwin.zsh
+++ tag-osx/exports.darwin.zsh
@@ -12,7 +12,7 @@
 }
 
 [[ -d /usr/local/opt/ruby/bin ]] && {
-  export PATH="/usr/local/opt/ruby/bin:${PATH}"
+  export PATH="/usr/local/opt/ruby/bin:/usr/local/lib/ruby/gems/2.6.0/bin:${PATH}"
   export LDFLAGS="-L/usr/local/opt/ruby/lib"
   export CPPFLAGS="-I/usr/local/opt/ruby/include"
   export PKG_CONFIG_PATH="/usr/local/opt/ruby/lib/pkgconfig"
