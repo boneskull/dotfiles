f364cf10c7abcaefc06cd88cb5168aa1d24f6619
Christopher Hiller <boneskull@boneskull.com>
2022-06-28 13:15:31 -0700

    remove cruft from exports
    - `Server.app` isn't a thing anymore
    - I don't think `ccache` lives there any longer
    - `nnn` is befuddling

--- tag-osx/exports.darwin.zsh
+++ tag-osx/exports.darwin.zsh
@@ -2,15 +2,6 @@
   export PATH="/opt/X11/bin:${PATH}"
 }
 
-[[ -d /Applications/Server.app/ ]] && {
-  # importantly, don't override homebrew with this crap
-  export PATH="${PATH}:/Applications/Server.app/Contents/ServerRoot/usr/bin:/Applications/Server.app/Contents/ServerRoot/usr/sbin:"
-}
-
-[[ -d /usr/local/opt/ccache/libexec ]] && {
-  export PATH="/usr/local/opt/ccache/libexec:${PATH}"
-}
-
 [[ -d /usr/local/opt/ruby/bin ]] && {
   export PATH="/usr/local/opt/ruby/bin:/usr/local/lib/ruby/gems/2.6.0/bin:${PATH}"
   export LDFLAGS="-L/usr/local/opt/ruby/lib"
@@ -26,9 +17,6 @@
   [[ -d $(brew --prefix)/share/zsh/help ]] && {
     export HELPDIR=$(brew --prefix)/share/zsh/help
   }
-
-[[ $(get-env nnn) ]] && {
-  export NNN_BMS="d:${HOME}/downloads;p:${HOME}/projects"
 }
 
 # Always enable colored `grep` output (bsd grep, I guess)
