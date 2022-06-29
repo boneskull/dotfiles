12f4e40bf14e446ace5affa770e78b7fc7b0c36f
Christopher Hiller <boneskull@boneskull.com>
2022-06-29 15:34:02 -0700

    comment out ruby stuff I probably don't need

--- tag-osx/exports.darwin.zsh
+++ tag-osx/exports.darwin.zsh
@@ -2,13 +2,6 @@
   export PATH="/opt/X11/bin:${PATH}"
 }
 
-[[ -d /usr/local/opt/ruby/bin ]] && {
-  export PATH="/usr/local/opt/ruby/bin:/usr/local/lib/ruby/gems/2.6.0/bin:${PATH}"
-  export LDFLAGS="-L/usr/local/opt/ruby/lib"
-  export CPPFLAGS="-I/usr/local/opt/ruby/include"
-  export PKG_CONFIG_PATH="/usr/local/opt/ruby/lib/pkgconfig"
-}
-
 [[ -d /opt/homebrew ]] && {
   eval "$(/opt/homebrew/bin/brew shellenv)"
   [[ -d $(brew --prefix)/share/antigen/ ]] && {
@@ -24,6 +17,14 @@
   [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 }
+
+# uncomment if ruby needed?
+# [[ -d /usr/local/opt/ruby/bin ]] && {
+#   export PATH="/usr/local/opt/ruby/bin:/usr/local/lib/ruby/gems/2.6.0/bin:${PATH}"
+#   export LDFLAGS="-L/usr/local/opt/ruby/lib"
+#   export CPPFLAGS="-I/usr/local/opt/ruby/include"
+#   export PKG_CONFIG_PATH="/usr/local/opt/ruby/lib/pkgconfig"
+# }
 
 # Always enable colored `grep` output (bsd grep, I guess)
 export GREP_OPTIONS="--color=auto"
