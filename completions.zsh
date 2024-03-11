5f319841667a86cd24ed252d7e6df7b8939c32b9
Christopher Hiller <boneskull@boneskull.com>
2024-03-11 11:57:44 -0700

    chore(completions): add graphite cli completions

--- completions.zsh
+++ completions.zsh
@@ -27,3 +27,22 @@ _yargs_completions() {
 }
 complete -F _yargs_completions nps
 ###-end-nps-completions-###
+
+#compdef gt
+###-begin-gt-completions-###
+#
+# yargs command completion script
+#
+# Installation: gt completion >> ~/.zshrc
+#    or gt completion >> ~/.zprofile on OSX.
+#
+_gt_yargs_completions() {
+  local reply
+  local si=$IFS
+  IFS=$'
+' reply=($(COMP_CWORD="$((CURRENT - 1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" gt --get-yargs-completions "${words[@]}"))
+  IFS=$si
+  _describe 'values' reply
+}
+compdef _gt_yargs_completions gt
+###-end-gt-completions-###
