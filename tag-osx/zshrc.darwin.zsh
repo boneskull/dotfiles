64f56e127bce3031ca7d38e13a879dcd42e3dcd0
Christopher Hiller <boneskull@boneskull.com>
2024-06-21 12:55:40 -0700

    fix: add ulimit shiz for macos

--- tag-osx/zshrc.darwin.zsh
+++ tag-osx/zshrc.darwin.zsh
@@ -5,3 +5,13 @@
   trysource $(brew --prefix)/etc/brew-wrap
   trysource $(brew --prefix)/etc/profile.d/z.sh
 }
+
+# https://stackoverflow.com/questions/45004352/error-enfile-file-table-overflow-scandir-while-run-reaction-on-mac
+#
+# may need to run this as root:
+#
+# echo kern.maxfiles=524288 | sudo tee -a /etc/sysctl.conf
+# echo kern.maxfilesperproc=524288 | sudo tee -a /etc/sysctl.conf
+# sudo sysctl -w kern.maxfiles=524288
+# sudo sysctl -w kern.maxfilesperproc=524288
+ulimit -n 524288 524288
