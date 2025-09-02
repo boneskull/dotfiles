d342c3ddf4d78ca3c134229bc64f6b1df07f7f59
Christopher Hiller <boneskull@boneskull.com>
2025-09-02 11:53:20 -0700

    chore: remove fix-lockfile alias

--- aliases.zsh
+++ aliases.zsh
@@ -11,5 +11,3 @@ alias reload="exec $SHELL -l"
 alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
 
 alias kn='killall node'
-
-alias fix-lockfile='trash node_modules packages/*/node_modules; npm i && npm ls && git add -A package-lock.json'
