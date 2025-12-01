84c2cbe056ce311af4f57f53d991de07ef626ef8
Christopher Hiller <boneskull@boneskull.com>
2025-12-01 15:34:21 -0800

    fix(completions): remove dead nps completions
    - Remove hardcoded nps (npm-package-scripts) completions
    - Keep gt (Graphite) completions, wrapped in command check
    - Simplify completions.zsh significantly

--- completions.zsh
+++ completions.zsh
@@ -1,48 +1,16 @@
 autoload -U +X compinit && compinit
 autoload -U +X bashcompinit && bashcompinit
 
-###-begin-nps-completions-###
-#
-# yargs command completion script
-#
-# Installation: node_modules/.bin/nps completion >> ~/.bashrc
-#    or node_modules/.bin/nps completion >> ~/.bash_profile on OSX.
-#
-_yargs_completions() {
-  local cur_word args type_list
-
-  cur_word="${COMP_WORDS[COMP_CWORD]}"
-  args=("${COMP_WORDS[@]}")
-
-  # ask yargs to generate completions.
-  type_list=$(node_modules/.bin/nps --get-yargs-completions "${args[@]}")
-  COMPREPLY=($(compgen -W "${type_list}" -- ${cur_word}))
-
-  # if no match was found, fall back to filename completion
-  if [ ${#COMPREPLY[@]} -eq 0 ]; then
-    COMPREPLY=($(compgen -f -- "${cur_word}"))
-  fi
-
-  return 0
+# gt (Graphite) completions - generated via `gt completion zsh`
+# Alternative: eval "$(gt completion zsh)" but that adds ~50ms startup time
+(( $+commands[gt] )) && {
+  #compdef gt
+  _gt_yargs_completions() {
+    local reply
+    local si=$IFS
+    IFS=$'\n' reply=($(COMP_CWORD="$((CURRENT - 1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" gt --get-yargs-completions "${words[@]}"))
+    IFS=$si
+    _describe 'values' reply
+  }
+  compdef _gt_yargs_completions gt
 }
-complete -F _yargs_completions nps
-###-end-nps-completions-###
-
-#compdef gt
-###-begin-gt-completions-###
-#
-# yargs command completion script
-#
-# Installation: gt completion >> ~/.zshrc
-#    or gt completion >> ~/.zprofile on OSX.
-#
-_gt_yargs_completions() {
-  local reply
-  local si=$IFS
-  IFS=$'
-' reply=($(COMP_CWORD="$((CURRENT - 1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" gt --get-yargs-completions "${words[@]}"))
-  IFS=$si
-  _describe 'values' reply
-}
-compdef _gt_yargs_completions gt
-###-end-gt-completions-###
