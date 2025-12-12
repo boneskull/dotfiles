cab0f460d747070e1f91816af045d4cd18492401
Christopher Hiller <boneskull@boneskull.com>
2025-12-12 14:39:00 -0800

    chore(osx): remove cruft

--- tag-osx/functions.darwin.zsh
+++
@@ -1,3 +0,0 @@
-# macOS-specific functions
-# Note: functions in tag-osx/zshfunctions/ are merged into ~/.zshfunctions by rcm
-# and autoloaded by functions.zsh, so no additional loading is needed here.
