d69a3ea39fdf45f814f839ff2f30fca9b5341a6d
Christopher Hiller <boneskull@boneskull.com>
2025-12-12 14:39:50 -0800

    chore: cleanup aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,22 +1,33 @@
-# Reload the shell (i.e. invoke as a login shell)
-alias reload="exec $SHELL -l"
-
-(( $+commands[bat] )) && {
+if (( $+commands[bat] )); then
   alias cat="bat --paging=never"
   alias more="bat"
-} || {
+else
   alias more="less -X"
-}
+fi
 
-alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
+(( $+commands[dig] )) && {
+  alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
+}
 
 (( $+commands[eza] )) && {
   alias ls="eza --icons --git"
   alias ltr="ls -l --sort=modified"
 }
 
-alias kn='killall node'
+# for killing scripts that refuse to die, such as AVA in a debug session or a sync benchmark
+alias kn='killall -9 node'
 
 (( $+commands[claude] )) && {
   alias claude="claude --dangerously-skip-permissions"
+}
+
+# VS Code shortcut
+# (( $+commands[code] )) && {
+#   alias c=code
+# }
+
+
+# Cursor shortcut
+(( $+commands[cursor] )) && {
+  alias c=cursor
 }
