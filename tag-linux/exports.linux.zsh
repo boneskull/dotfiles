c468ad4d8832442a2537edb9a8e2fda2bcbf41dd
Christopher Hiller <boneskull@boneskull.com>
2025-12-12 14:36:29 -0800

    chore(linux): move linuxbrew shit into tag

--- tag-linux/exports.linux.zsh
+++ tag-linux/exports.linux.zsh
@@ -1,2 +1,7 @@
 # Linux-specific exports
 # (debian/apt and systemd detection now handled by antidote plugins)
+
+# setup linuxbrew
+[[ -d /home/linuxbrew/.linuxbrew ]]; then
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
