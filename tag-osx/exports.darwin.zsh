89e0a5182f5436155ad284ff1834f330cddada66
Christopher Hiller <boneskull@boneskull.com>
2025-12-12 14:38:53 -0800

    chore(osx): exports cleanup

--- tag-osx/exports.darwin.zsh
+++ tag-osx/exports.darwin.zsh
@@ -1,26 +1,29 @@
 # macOS-specific exports
 
-[[ -d /opt/X11/ ]] && export PATH="/opt/X11/bin:${PATH}"
-
 export PATH="${HOME}/.local/bin:${PATH}"
 
-# Homebrew-specific setup (HOMEBREW_PREFIX set by brew shellenv in exports.zsh)
-[[ -n "${HOMEBREW_PREFIX}" ]] && {
-  # Zsh help files from homebrew
-  [[ -d "${HOMEBREW_PREFIX}/share/zsh/help" ]] && export HELPDIR="${HOMEBREW_PREFIX}/share/zsh/help"
+function homebrew_exports {
+  local HOMEBREW_PREFIX="${1}"
+  # Homebrew-specific setup (HOMEBREW_PREFIX set by brew shellenv in exports.zsh)
+  if [[ -n "${HOMEBREW_PREFIX}" ]]; then
+    # Zsh help files from homebrew
+    [[ -d "${HOMEBREW_PREFIX}/share/zsh/help" ]] && export HELPDIR="${HOMEBREW_PREFIX}/share/zsh/help"
 
-  # leaving this enabled makes everything slow af
-  export HOMEBREW_NO_INSTALL_CLEANUP=1
+    # leaving this enabled makes everything slow af
+    export HOMEBREW_NO_INSTALL_CLEANUP=1
+    export HOMEBREW_NO_AUTO_UPDATE=1
 
-  # NVM via homebrew (oh-my-zsh nvm plugin handles lazy loading)
-  [[ -z "${NVM_DIR}" && -d "${HOMEBREW_PREFIX}/opt/nvm" ]] && export NVM_DIR="${HOMEBREW_PREFIX}/opt/nvm"
+    # NVM via homebrew (oh-my-zsh nvm plugin handles lazy loading)
+    (( $+commands[nvm] )) && : ${NVM_DIR:="${HOMEBREW_PREFIX}/opt/nvm"}
+  else
+    echo "HOMEBREW_PREFIX is not set! Skipping Homebrew exports." >&2
+  fi
 }
+
+(( $+commands[brew] )) && homebrew_exports "$(brew --prefix)"
 
 # Always enable colored `grep` output
 export GREP_OPTIONS="--color=auto"
-
-# Android SDK
-[[ -d "${HOME}/Library/Android" ]] && export ANDROID_HOME="${HOME}/Library/Android/sdk"
 
 # Use vivid for LS_COLORS if installed
 (( $+commands[vivid] )) && export LS_COLORS="$(vivid generate lava)"
