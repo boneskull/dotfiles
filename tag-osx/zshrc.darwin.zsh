ec23ed77cc9d892fa58953d0c594af5accac8d6e
Christopher Hiller <boneskull@boneskull.com>
2025-12-12 14:39:10 -0800

    chore(osx): cleanup zshrc

--- tag-osx/zshrc.darwin.zsh
+++ tag-osx/zshrc.darwin.zsh
@@ -1,10 +1,28 @@
 # zshrc for darwin
 
-[[ -n "${HOMEBREW_PREFIX}" ]] && {
+function setup_homebrew {
+  local HOMEBREW_PREFIX="${1}"
   # setup for brew-file
-  trysource "${HOMEBREW_PREFIX}/etc/brew-wrap"
-  trysource "${HOMEBREW_PREFIX}/etc/profile.d/z.sh"
+  if [[ -n "${HOMEBREW_PREFIX}" ]]; then
+    trysource "${HOMEBREW_PREFIX}/etc/brew-wrap"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  else
+    echo "HOMEBREW_PREFIX is not set! Skipping Homebrew setup." >&2
+  fi
 }
+
+# initialize z
+function setup_z {
+  local HOMEBREW_PREFIX="${1}"
+  if [[ -n "${HOMEBREW_PREFIX}" ]]; then
+    trysource "${1}/etc/profile.d/z.sh"
+  else
+    echo "HOMEBREW_PREFIX is not set! Skipping z setup." >&2
+  fi
+}
+
+(( $+commands[brew] )) && setup_homebrew "$(brew --prefix)"
+(( $+commands[z] )) && setup_z "$(brew --prefix)"
 
 # https://stackoverflow.com/questions/45004352/error-enfile-file-table-overflow-scandir-while-run-reaction-on-mac
 #
